4f7bf54d56c44c0ffb4f7617f2774a9909adfbdc
Huzaif <mhuzaif427@gmail.com>
2024-12-23 22:15:38 +0530

    Initial commit

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <div className="mt-6 p-6 text-gray-500 text-center text-[13px]">
-            Made by @jansm04 in 2024&nbsp; •&nbsp; 
-            {<Link href='https://github.com/jansm04/dav' target="_blank">
+            Made by Huzaif in 2024&nbsp; •&nbsp; 
+            {<Link href='https://github.com/Huzaif2309' target="_blank">
                 <div className="text-sky-700 inline"> Github </div>
             </Link>}
         </div>
